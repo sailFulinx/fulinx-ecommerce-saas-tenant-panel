388fc039d643c784008b14361f46fb4be68d15d8
fulinx <sail.fulinx@gmail.com>
2024-12-06 18:12:56 +0800

    feat: product

--- src/views/Product/Show.vue
+++ src/views/Product/Show.vue
@@ -1,8 +1,5 @@
 <script setup name="ProductDetail" lang="ts">
-import {
-  showProductApi,
-} from '@/api/product'
-import { getProductTypeLabel, productTypes } from '@/data/product'
+import { showProductApi } from '@/api/product'
 import { useLocale } from '@/hooks/useLocale'
 import { usePreferenceStore } from '@/stores/preference'
 import { ElAlert, ElCard, ElForm, ElInput, ElMessage, ElSwitch, ElTabPane } from 'element-plus'
@@ -19,127 +16,6 @@ const loading = reactive({
   init: false,
   list: false,
 })
-
-const listProductValueQuery = reactive<ProductProductValueListParams & Pagination>({
-  languageId: usePreferenceStore().preference?.language.id,
-  productId: id,
-  pageSize: 20,
-  pageNumber: 1,
-})
-const selectedProductValueList = ref<string[]>([])
-
-const selectedProductValueItem = (val: (ProductValueListData & CommonField)[]) => {
-  selectedProductValueList.value = []
-  val.forEach(item => {
-    selectedProductValueList.value.push(item.id)
-  })
-}
-
-const listProductValueResult = ref<TableResponse<ProductValueListData & CommonField>>({
-  list: [],
-  total: 0,
-})
-
-const getProductValueList = async () => {
-  loading.list = true
-  const { data } = await productValueListApi(listProductValueQuery).catch(err => {
-    loading.list = false
-    throw err
-  })
-  listProductValueResult.value = data
-  loading.list = false
-}
-
-const paginationProductValue = (val: PaginationComponentDataType) => {
-  if (val) {
-    listProductValueQuery.pageSize = val.limit
-    listProductValueQuery.pageNumber = val.page
-  }
-  getProductValueList()
-}
-
-const productValueDialogRef = ref()
-
-const productValueFormRef = ref()
-
-const productValueDialogVisible = ref(false)
-
-const handleCreateProductValue = () => {
-  productValueDialogVisible.value = true
-}
-
-const productValueForm = reactive<CreateProductValueParams>({
-  productId: id,
-  productValueDetailId: '',
-  productValueContent: '',
-  languageId: selectLanguage.value.id,
-})
-
-const productValueFormRules = {
-  productValueContent: [
-    { required: true, message: $t('product.placeholder.productValueContent'), trigger: 'blur' },
-  ],
-}
-
-const isEditProductValue = ref<boolean>(false)
-
-const handleEditProductValue = (item: ProductValueListData & CommonField) => {
-  productValueForm.productValueContent = item.productValueContent
-  productValueForm.productValueDetailId = item.productValueDetailId
-  productValueDialogVisible.value = true
-  isEditProductValue.value = true
-}
-
-const handleSubmitProductValue = async () => {
-  const isValid = await useValidForm(productValueFormRef.value)
-  console.log(isValid)
-  if (!isValid) {
-    return
-  }
-  productValueForm.productValueContent = productValueForm.productValueContent.trim()
-  loading.list = true
-  if (!isEditProductValue.value) {
-    await createProductValueApi(productValueForm).catch(error => {
-      loading.list = false
-      throw error
-    })
-  } else {
-    await updateProductValueDetailProductValueContentApi(productValueForm).catch(error => {
-      loading.list = false
-      throw error
-    })
-  }
-  isEditProductValue.value = false
-  productValueDialogVisible.value = false
-  await getProductValueList()
-}
-
-const handleDeleteProductValue = async (item: ProductValueListData & CommonField) => {
-  const deletedIds = [item.id]
-  loading.list = true
-  await removeProductValueApi({
-    productValueIds: deletedIds,
-  }).catch(error => {
-    loading.list = false
-    throw error
-  })
-  await getProductValueList()
-  ElMessage.success($t('success.remove'))
-}
-
-const handleMultiDeleteProductValue = async () => {
-  const deletedIds = selectedProductValueList.value
-  loading.list = true
-  await removeProductValueApi({
-    productValueIds: deletedIds,
-  }).catch(error => {
-    loading.list = false
-    throw error
-  })
-  await getProductValueList()
-  selectedProductValueList.value = []
-  ElMessage.success($t('success.remove'))
-}
 
 const handleChangeTab = async (pane: string) => {
   if (pane === 'productValue') {
@@ -183,7 +59,7 @@ const showProductPayload = reactive<ShowProductParams>({
   languageId: selectLanguage.value.id,
 })
 
-// 获取文章数据
+// 获取数据
 const getProductData = async () => {
   loading.init = true
   const { data } = await showProductApi(showProductPayload).catch(error => {
@@ -267,22 +143,6 @@ const createProductName = async () => {
   await resetFormData(data)
   ElMessage.success($t('success.create'))
 }
-
-const editProductTypeVisible = ref<boolean>(false)
-
-// 更新参数类型
-const editProductType = async () => {
-  const payload = {
-    productId: id,
-    languageId: selectLanguage.value.id,
-    productType: form.productType,
-  }
-  await updateProductTypeApi(payload).catch(error => {
-    throw error
-  })
-  ElMessage.success($t('success.edit'))
-  editProductTypeVisible.value = false
-}
 </script>
 
 <template>
@@ -331,35 +191,6 @@ const editProductType = async () => {
                   </div>
                 </div>
               </div>
-              <div class="w-full grid grid-cols-12 gap-8 p-4 border-b border-gray-200">
-                <div class="col-span-1 font-semibold fs-[14px] text-gray-700">
-                  {{ $t('product.productType') }}:
-                </div>
-                <div class="col-span-11 w-full flex items-center">
-                  <div v-if="!editProductTypeVisible" class="mr-2 flex">
-                    <div class="mr-1">
-                      {{ getProductTypeLabel(form.productType) }}
-                    </div>
-                    <EBtn
-                      type="primary"
-                      text
-                      @click="editProductTypeVisible = true"
-                    >
-                      <Icon icon="ep:edit" :size="4" class="mr-1" />
-                    </EBtn>
-                  </div>
-                  <div v-else>
-                    <ElSelect v-model="form.productType" :placeholder="$t('product.placeholder.productType')" style="width:120px" @change="editProductType">
-                      <ElOption
-                        v-for="item in productTypes"
-                        :key="item.id"
-                        :label="item.label"
-                        :value="item.id"
-                      />
-                    </ElSelect>
-                  </div>
-                </div>
-              </div>
             </div>
           </ElCard>
           <ElCard v-else>
@@ -375,79 +206,7 @@ const editProductType = async () => {
             </div>
           </ElCard>
         </ElTabPane>
-        <ElTabPane v-if="form.productType === 1 || form.productType === 3" :label="$t('product.productValue')" name="productValue">
-          <div class="flex justify-between items-center mb-5">
-            <div>
-              参数值列表
-            </div>
-            <div>
-              <EBtn type="primary" @click="handleCreateProductValue">
-                添加参数值
-              </EBtn>
-              <EBtn type="danger" @click="handleMultiDeleteProductValue">
-                删除参数值
-              </EBtn>
-            </div>
-          </div>
-          <ElTable
-            v-loading="loading.list"
-            :data="listProductValueResult.list"
-            row-key="id"
-            tooltip-effect="dark"
-            default-expand-all
-            highlight-current-row
-            border
-            @selection-change="selectedProductValueItem"
-          >
-            <ElTableColumn type="selection" width="55" />
-            <ElTableColumn :label="$t('product.productValueContent')">
-              <template #default="scope">
-                <span>{{ scope.row.productValueContent }}</span>
-              </template>
-            </ElTableColumn>
-            <ElTableColumn label="操作" header-align="center" width="220" align="center" class-name="pl-15 fixed-width">
-              <template #default="scope">
-                <span class="mr-5">
-                  <EBtn size="small" @click="handleEditProductValue(scope.row)">
-                    <Icon icon="ep:edit" class="mr-1" />
-                    {{ $t('common.edit') }}
-                  </EBtn>
-                </span>
-                <span>
-                  <EBtn size="small" type="danger" @click="handleDeleteProductValue(scope.row)">
-                    <Icon icon="ep:delete" class="mr-1" />
-                    {{ $t('common.remove') }}
-                  </EBtn>
-                </span>
-              </template>
-            </ElTableColumn>
-          </ElTable>
-          <Pagination
-            v-show="listProductValueResult.total > 0"
-            v-model:page="listProductValueQuery.pageNumber"
-            v-model:limit="listProductValueQuery.pageSize"
-            :total="listProductValueResult.total"
-            @pagination="paginationProductValue"
-          />
-        </ElTabPane>
       </ElTabs>
-      <ElDialog ref="productValueDialogRef" v-model="productValueDialogVisible" title="添加参数值">
-        <ElForm ref="productValueFormRef" :model="productValueForm" :rules="productValueFormRules" label-width="120px">
-          <ElFormItem :label="$t('product.productValueContent')" prop="productValueContent">
-            <ElInput v-model="productValueForm.productValueContent" :placeholder="$t('product.placeholder.productValueContent')" />
-          </ElFormItem>
-        </ElForm>
-        <template #footer>
-          <div class="dialog-footer">
-            <EBtn @click="productValueDialogVisible = false">
-              {{ $t('common.cancel') }}
-            </EBtn>
-            <EBtn type="primary" @click="handleSubmitProductValue">
-              {{ $t('common.submit') }}
-            </EBtn>
-          </div>
-        </template>
-      </ElDialog>
     </div>
   </div>
 </template>
