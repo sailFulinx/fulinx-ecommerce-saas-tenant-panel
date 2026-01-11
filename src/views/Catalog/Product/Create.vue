73e7f3e51d628c3743261b2e0f4713dc52c042d8
fulinx <sail.fulinx@gmail.com>
2026-01-11 17:20:39 +0800

    feat: product

--- src/views/Catalog/Product/Create.vue
+++ src/views/Catalog/Product/Create.vue
@@ -251,7 +251,34 @@ const save = async () => {
     <div class="view-main theme-card">
       <ElForm ref="productFormRef" :model="productForm" :rules="rules" label-width="140px">
         <div class="grid grid-cols-12 gap-4">
-          <div ref="containerRef" class="col-span-9" style="height: 300px; overflow-y: auto">
+          <div class="col-span-2">
+            <ElAnchor
+              v-model:active-href="currentHref"
+              :container="containerRef"
+              direction="vertical"
+              type="default"
+              :target-scroll="true"
+              @click="handleClickAnchor"
+            >
+              <ElAnchorLink
+                href="#productName"
+                :title="$t('product.productBasicInfo')"
+              />
+              <ElAnchorLink
+                href="#productFile"
+                :title="$t('product.imageVideo')"
+              />
+              <ElAnchorLink
+                href="#productOption"
+                :title="$t('product.specifications')"
+              />
+              <ElAnchorLink
+                href="#productOther"
+                :title="$t('product.otherInfo')"
+              />
+            </ElAnchor>
+          </div>
+          <div ref="containerRef" class="col-span-10 container-custom">
             <!-- 基础 -->
             <ElCard id="productName" shadow="never" class="mb-5">
               <template #header>
@@ -324,38 +351,15 @@ const save = async () => {
               其它
             </ElCard>
           </div>
-          <div class="col-span-3 vp-raw">
-            <ElAnchor
-              v-model:active-href="currentHref"
-              :offset="20"
-              :container="containerRef"
-              direction="vertical"
-              type="default"
-              :target-scroll="true"
-              @click="handleClickAnchor"
-            >
-              <ElAnchorLink
-                href="#productName"
-                :title="$t('product.productBasicInfo')"
-              />
-              <ElAnchorLink
-                href="#productFile"
-                :title="$t('product.imageVideo')"
-              />
-              <ElAnchorLink
-                href="#productOption"
-                :title="$t('product.specifications')"
-              />
-              <ElAnchorLink
-                href="#productOther"
-                :title="$t('product.otherInfo')"
-              />
-            </ElAnchor>
-          </div>
         </div>
       </ElForm>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .container-custom {
+    height: calc(100vh - 210px);
+    overflow-y: auto;
+  }
+</style>
